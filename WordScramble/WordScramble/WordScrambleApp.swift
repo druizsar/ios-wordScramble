0b11b53439204c6613967a5d292c95bf59caba1d
David Ruiz Sarrazola <druizsar@gmail.com>
2023-02-16 13:00:01 -0500

    Initial implementation

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by David Ruiz on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
